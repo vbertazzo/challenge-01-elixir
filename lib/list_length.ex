dd31830f274fccecd17a382045fd016e4324b922
vbertazzo <vbertazzo@gmail.com>
2022-01-06 01:26:18 -0300

    Add ListLength call function

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,15 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list) do
+    list_length(list, 0)
+  end
+
+  def list_length([], acc) do
+    acc
+  end
+
+  def list_length(list, acc) do
+    acc = acc + 1
+    [_ | tail] = list
+    list_length(tail, acc)
   end
 end
